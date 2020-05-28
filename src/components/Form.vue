f57c5d315512ed6c2484835c398eb1724e6319a9
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-28 13:51:22 -0400

    Estilos de componente hijo

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="form container">
+  <div class="form container text-center">
     
-    <section class="tareas">
+    <h3>Listado de Tareas</h3>
+    <section class="tareas d-flex flex-row justify-content-center mt-4">
       <ul>
-        <li v-for="item in data" :key="item.id">
-          {{ item }}
+        <li v-for="item in data" :key="item.id" class="py-2">
+          {{ item }} <button class="btn btn-danger btn-sm ml-3">Eliminar</button>
         </li>
-        <button class="btn btn-danger btn-md">Eliminar</button>
+        
       </ul>
     </section>
   </div>
