8195920bd2da4ecff92ab79e6717071463cc31da
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-28 13:37:50 -0400

    Componente padre e hijo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <form @submit.prevent="agregar" @reset="limpiar">
+      <div class="form-group">
+        <label>Tarea</label>
+        <input v-model="tareas" type="text" /> type="text" class="form-control" placeholder="Ingrese nueva tarea">
+      </div>
+      <button type="submit" class="btn btn-success">Añadir</button>
+      <button type="reset" class="btn btn-warning">Limpiar</button>
+    </form>
+
+
+    <Form :data=this.tareas></Form>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Form from "./components/Form.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Form
+  },
+  data() {
+    return {
+      tareas: []
+    }
+  },
+  methods: {
+    agregar() {
+
+    },
+    limpiar() {
+      this.tarea = ""
+    },
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
---
+++ src/components/Form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form container">
+    
+    <section class="tareas">
+      <ul>
+        <li v-for="item in data" :key="item.id">
+          {{ item }}
+        </li>
+        <button class="btn btn-danger btn-md">Eliminar</button>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  props: {
+    data: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    eliminarTarea() {
+
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
